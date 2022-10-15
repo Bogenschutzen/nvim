6de755a781d24c6077ed764199a7938c62c5821f
loveafy <96562010+loveafy@users.noreply.github.com>
2022-10-15 11:36:17 +0800

    Create init.lua

---
+++ lua/configure/init.lua
@@ -0,0 +1,9 @@
+require "configure.dashboard"
+require "configure.nvim-tree"
+require "configure.lualine"
+require "configure.symbols"
+require "configure.cmp"
+require "configure.lsp"
+require "configure.nvim-treesitter"
+
+--require 'lspconfig'.ccls.setup{}
